f779e1f2dd6943befd1659ac99d37ee87f769196
Allison <aalvarado40731@ufide.ac.cr>
2025-06-25 20:51:14 -0600

    Add files via upload

---
+++ Casoevaluado1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package casoevaluado1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 50672
+ */
+public class Casoevaluado1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Datos del cliente
+        String nombreCliente = JOptionPane.showInputDialog("Ingrese el nombre del cliente:");
+        String cedulaCliente = JOptionPane.showInputDialog("Ingrese la cédula del cliente:");
+        String codigoFactura = JOptionPane.showInputDialog("Ingrese el código de factura del cliente:");
+        int mes = JOptionPane.showConfirmDialog(null, "Ingrese el numero de mes indicado:");
+        String productosElectricos = JOptionPane.showInputDialog("Indique si la factura tiene productos electricos");
+        String productosAutomotrices = JOptionPane.showInputDialog("Indique si la factura tiene productos automotrices");
+        String productosConstruccion = JOptionPane.showInputDialog("Indique si la factura tiene productos construccion");
+        double montoFacturado;
+        
+        
+        
+}
+    }
+
+
+
+
+      
+    
+
